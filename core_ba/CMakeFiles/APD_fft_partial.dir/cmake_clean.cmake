16a6309738a3c3c851de401a886d8079d74014c2
mzvic <vmontesinos2021@udec.cl>
2023-07-24 19:11:27 -0400

    mas estable y con opción a 0.1Hz de resolución

---
+++ core_ba/CMakeFiles/APD_fft_partial.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/APD_fft_partial.dir/APD_fft_partial.cc.o"
+  "bin/APD_fft_partial"
+  "bin/APD_fft_partial.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/APD_fft_partial.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ core_ba/CMakeFiles/APD_reg_fft_1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/APD_reg_fft_1.dir/APD_reg_fft_1.cc.o"
+  "bin/APD_reg_fft_1"
+  "bin/APD_reg_fft_1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/APD_reg_fft_1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
