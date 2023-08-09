4107ef57dd415d241b4b439f7230e671348ac7fe
mzvic <mzvic>
2023-08-09 17:56:42 -0400

    update01

--- core_ba/core_pb2_grpc.py
+++ core_ba/core_pb2_grpc.py
@@ -2,7 +2,7 @@
 """Client and server classes corresponding to protobuf-defined services."""
 import grpc
 
-import core_pb2 as core__pb2
+import core_ba.core_pb2 as core__pb2
 from google.protobuf import empty_pb2 as google_dot_protobuf_dot_empty__pb2
 
 
