c3e8690f06383a43eb5e774ed95629f1b623f289
mzvic <vmontesinos2021@udec.cl>
2023-09-03 02:35:30 -0300

    comentarios en modulos APD

--- core_ba/APD_reg_fft_1_code_sw.cc
+++ core_ba/APD_reg_fft_1_code_sw.cc
@@ -14,28 +14,39 @@ using namespace core;
 using namespace std::chrono;
 using namespace google::protobuf;
 
+// String variables to write averages to the file
 std::string data_freq;
 std::string data_fft;
 
+// Window length in the FFT
 int fft_frequencies = 0;
+
+// Count for FFT averaging operation
 int c = 0;
+
+// Variable to see if the period in which the FFT average is obtained has passed
 long int t_0 = 0;
+
+// Period to average (in seconds)
 int T;
 
+// Vectors related to obtaining FFT averages and indexes to eliminate (zeros)
 std::vector<float> data_freq_avg;
 std::vector<float> data_fft_avg;
 std::vector<int> delete_index;
 
+// Vector containing the values of the bundle
 vector<float> subs_values(fft_frequencies);
 
-bool exit_flag = false;
-std::mutex signal_mutex;
-std::condition_variable signal_cv;
+// Global variables
+bool exit_flag = false;  // Used to signal the program to exit
+std::mutex signal_mutex; // Mutex for synchronization
+std::condition_variable signal_cv; // Condition variable for synchronization
 
+// Variable for the name of the generated file
 char filename[60];
 
-const std::vector<int> interests = {4};
-
+// Function to handle the interrupt signal (SIGINT)
 void HandleSignal(int) {
     std::unique_lock<std::mutex> slck(signal_mutex);
     std::cout << "Exiting..." << std::endl;
@@ -43,34 +54,45 @@ void HandleSignal(int) {
     signal_cv.notify_one();
 }
 
+// Function that gets the FFT averages and stores them in the file
 void avg_fft(vector<float> subs_values, int fft_frequencies){
+    // File opens
     FILE *file = fopen(filename, "a");
+    
+    // Length is assigned to the frequency and magnitude vectors
     data_freq_avg.resize(fft_frequencies/2);
-    //data_freq_avg.assign(fft_frequencies, 0.0);
     data_fft_avg.resize(fft_frequencies/2);
-    //data_fft_avg.assign(fft_frequencies, 0.0);
+    
+    // The length of the bundle is obtained
     int bundle_size = subs_values.size();
-    //cout << "Bundle_size: "<< bundle_size << endl;
+    // If a bundle arrives, proceed
     if (bundle_size > 0) {
+        // The vector is restarted to obtain indices of zero values (values that are deleted)
         delete_index.resize(1);
         delete_index.assign(1, 0);
+        
+        // The timestamp of each average is obtained
         struct timeval tf;
         gettimeofday(&tf, nullptr);
         tf.tv_sec -= 14400;
 
+        // Values are assigned to the frequency vector
         for (float i = 0; i < fft_frequencies/2; ++i) {
             data_freq_avg[i] = subs_values[i];
             
         }
+        // A vector ('data_fft_avg') is generated with the accumulated magnitudes during the period indicated by the user
         for (float i = 0; i < (fft_frequencies/2); ++i) {// /10; ++i) {
             float fft_i = subs_values[i + fft_frequencies/2];          
             data_fft_avg[i] = data_fft_avg[i] + fft_i;
         }
-
         c += 1;
-        //std::cout << "C: " << c << std::endl;
+        
+        // Check if the period entered by the user has passed
         if ((tf.tv_sec >= t_0 + T) && (t_0 > 0)) {
+            // If the vector of accumulated values already has at least one value, proceed
             if (c > 0){
+                // Here the vector containing the indices to be eliminated (zero values) is generated and the accumulated values are divided by 'c', in this way the average is obtained 
                 for (int i = 0; i < data_fft_avg.size(); ++i) {
                     if (data_fft_avg[i] <= 0) {
                         delete_index.push_back(i);
@@ -79,52 +101,60 @@ void avg_fft(vector<float> subs_values, int fft_frequencies){
                     }
                 }
             }
+            // Zero values are deleted here (based on the 'delete_index' vector)
             for (int i = delete_index.size() - 1; i > 0; --i) {
                 int index = delete_index[i];
                 data_fft_avg.erase(data_fft_avg.begin() + index);
                 data_freq_avg.erase(data_freq_avg.begin() + index);
             }
             
+            // The timestamp is stored in the log file
             t_0 = tf.tv_sec;
             fprintf(file, "%ld.%06ld\n",  tf.tv_sec, tf.tv_usec% 1000000);
+            
+            // The magnitudes are stored in the log file
             for (int i = 0; i < data_fft_avg.size(); ++i) {
                 fprintf(file, "%f ", data_fft_avg[i]);
-                //std::cout << data_fft_avg[i] << " ";    
             }                
             fprintf(file, "\n");
+            
+            // Frequencies are stored in the log file
             for (int i = 0; i < data_freq_avg.size(); ++i) {
                 fprintf(file, "%f ", data_freq_avg[i]);
-                //std::cout << data_freq_avg[i] << " ";
             }
-            //std::cout << "Amount of spectrum averaged: " << c << std::endl;
-            
             fprintf(file, "\n\n");
+            
+            // The number of spectra contained in the average is printed (commented out for now)
+            //std::cout << "Amount of spectrum averaged: " << c << std::endl;
+
+            // Vectors related to FFT averaging are reset
             data_freq_avg.resize(fft_frequencies/2);
             data_freq_avg.assign(fft_frequencies/2, 0.0);
             data_fft_avg.resize(fft_frequencies/2);
             data_fft_avg.assign(fft_frequencies/2, 0.0);
+            
+            // The count of averaged spectra is restarted
             c = 0;
-        } else if (t_0 == 0) {
+        } else if (t_0 == 0) { // If the period entered by the user still does not pass, the time variable is updated (variable to check if it is possible to proceed to the previous if)
             t_0 = tf.tv_sec;          
         }  
     }
+    // The file is closed
     fclose(file);
 }
 
+// Function in which the bundle is received from the broker and saved in a vector to later be processed in the previous function
 void ProcessSub(const Bundle &bundle) {
-  //cout << "Starting ProcessSub..." << endl;
-  //fft_frequencies = bundle.value().size(); 
   while (fft_frequencies == 0){
     fft_frequencies = bundle.value().size();
   }
   
-  //cout << fft_frequencies << endl; 
   subs_values.assign(fft_frequencies, 0.0);
   
   for (int i = 0; i < fft_frequencies; i++) {
 	subs_values[i] = bundle.value(i);
-	//cout << "   " << bundle.value(i); 
   }  
+  
   avg_fft(subs_values, fft_frequencies); 
 }
 
@@ -133,25 +163,32 @@ int main(int argc, char *argv[]) {
         std::cout << "Usage: " << argv[0] << " T" << std::endl;
         return 1;
     }
-
+    
+    // Period over which averages are obtained (entered by the user through the GUI)
     T = std::stoi(argv[1]);
     std::unique_lock<std::mutex> slck(signal_mutex);
 
+    // Obtains the time at which data recording begins 
     struct timespec ts;
     timespec_get(&ts, TIME_UTC);
     ts.tv_sec -= 14400;
     struct tm t;
     gmtime_r(&ts.tv_sec, &t);
+    
+    // File name format
     char buf[50];
     strftime(buf, sizeof(buf), "[1Hz_res]FFT_avg_logs_%d-%m-%Y_%H:%M:%S", &t);
     printf("Creating [1Hz res] FFT log file...");
     snprintf(filename, sizeof(filename), "%s.txt", buf);
+    
+    // The file is opened to check that it was created correctly    
     FILE *file = fopen(filename, "w");
-    if (file == NULL) {
-        printf("Error creating FFT avg. log file...\n");
-    }
-    printf("Created file '%s'...\n", filename);
-   
+    if (file == NULL){
+        printf("Error creating FFT log file...\n");
+    }else{
+        printf("Created file '%s'...\n", filename);}
+    
+    // The function that receives the bundle for processing is called   
     SubscriberClient subscriber_client(&ProcessSub, vector<int>{DATA_FFT_PARTIAL});
      
     std::signal(SIGINT, HandleSignal);
--- core_ba/APD_reg_zero_code_sw.cc
+++ core_ba/APD_reg_zero_code_sw.cc
@@ -15,97 +15,106 @@ using namespace core;
 using namespace std::chrono;
 using namespace google::protobuf;
 
-bool exit_flag = false;
-std::mutex signal_mutex;
-std::condition_variable signal_cv;
+// Global variables
+bool exit_flag = false;  // Used to signal the program to exit
+std::mutex signal_mutex; // Mutex for synchronization
+std::condition_variable signal_cv; // Condition variable for synchronization
 
+// Variable for the name of the generated file
 char filename[70];
+
+// Bundle size variables
 int apd_size = 0;
 vector<int> subs_values(apd_size);
+
+// Time variables for logs
 int64_t sec_0 = 9999999999;
 int32_t usec_0 = 999999999;
 int64_t sec_value;
 int32_t nsec_value;
 
-void HandleSignal(int)
-{
+// Function to handle the interrupt signal (SIGINT)
+void HandleSignal(int){
     std::unique_lock<std::mutex> slck(signal_mutex);
     std::cout << "Exiting..." << std::endl;
     exit_flag = true;
     signal_cv.notify_one();
 }
 
-void Subscribe(int64_t sec_value, int32_t nsec_value, std::vector<int> subs_values, int apd_size)
-{
+// Function to save logs
+void Subscribe(int64_t sec_value, int32_t nsec_value, std::vector<int> subs_values, int apd_size){
+    // File opens
     FILE *file = fopen(filename, "a");
     int64_t sec_i = 0;
     int32_t usec_i = 0;
 
-    if (sec_value < sec_0)
-    {
+    // The start time of registers is assigned (to subtract it from the instantaneous time and thus obtain registers starting from T = 0)
+    if (sec_value < sec_0){
         sec_0 = sec_value;
         usec_0 = nsec_value;
     }
-
-    for (int i = 0; i < subs_values.size(); ++i)
-    {
+    
+    // Operation to obtain the counts values and send them to the broker (@ 100kHz)
+    for (int i = 0; i < subs_values.size(); ++i){
+        
+        // Operations to assign the appropriate timestamp to each accumulated value
         sec_i = sec_value;
         usec_i = nsec_value + i * 9700;
         int64_t sec_diff = sec_i - sec_0;
         int32_t usec_diff = usec_i - usec_0;
-
-        if (usec_diff < 0)
-        {
+        if (usec_diff < 0){
             sec_diff--;
             usec_diff += 1000000000;
         }
+        
+        // Data is written to the file
         fprintf(file, "%ld.%09d;%d\n", sec_diff, usec_diff % 1000000000, subs_values.at(i));
-        // printf("%ld.%09d;%d\n", sec_diff, usec_diff % 1000000000, subs_values.at(i));
     }
+    // File is closed
     fclose(file);
 }
 
-void ProcessSub(const Bundle &bundle)
-{
-    // cout << "Starting ProcessSub..." << endl;
-    // apd_size = bundle.value().size();
+// Function in which the bundle is received from the broker (data and timestamp) and saved in a vector to later be saved in the previous function
+void ProcessSub(const Bundle &bundle){
     apd_size = 0;
-    while (apd_size == 0)
-    {
+    while (apd_size == 0){
         apd_size = bundle.value().size();
         sec_value = bundle.timestamp().seconds();
         nsec_value = bundle.timestamp().nanos();
     }
 
-    // cout << apd_size << endl;
     subs_values.assign(apd_size, 0.0);
 
-    for (int i = 0; i < apd_size; i++)
-    {
+    for (int i = 0; i < apd_size; i++){
         subs_values[i] = bundle.value(i);
-        // cout << "   " << bundle.value(i);
     }
     Subscribe(sec_value, nsec_value, subs_values, apd_size);
 }
 
-int main()
-{
+int main(){
     std::unique_lock<std::mutex> slck(signal_mutex);
+
+    // Obtains the time at which data recording begins    
     struct timespec ts;
     timespec_get(&ts, TIME_UTC);
     ts.tv_sec -= 14400;
     struct tm t;
     gmtime_r(&ts.tv_sec, &t);
+    
+    // File name format
     char buf[40];
     strftime(buf, sizeof(buf), "[100kHz]APD_logs_%d-%m-%Y_%H:%M:%S", &t);
     printf("Creating APD log file...");
     snprintf(filename, sizeof(filename), "%s.txt", buf);
+    
+    // The file is opened to check that it was created correctly    
     FILE *file = fopen(filename, "w");
-    if (file == NULL)
-    {
+    if (file == NULL){
         printf("Error creating APD log file...\n");
-    }
-    printf("Created file '%s'...\n", filename);
+    }else{
+        printf("Created file '%s'...\n", filename);}
+    
+    // The function that receives the bundle for processing is called    
     SubscriberClient subscriber_client(&ProcessSub, vector<int>{DATA_APD_FULL});
 
     std::signal(SIGINT, HandleSignal);
