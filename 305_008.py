0679c3367ee257a9af871c5f8580a6ab0c03e626
mzvic <vmontesinos2021@udec.cl>
2024-06-18 10:03:18 -0400

    config de twistorr actualizada

--- 305_008.py
+++ 305_008.py
@@ -1,4 +1,41 @@
+import sys
 import serial
+from PyQt5.QtWidgets import QApplication, QMessageBox
+from PyQt5 import QtGui, QtCore, QtWidgets
+from PyQt5.QtGui import QFont, QImage, QPixmap
+
+def pumpNotFound():
+    msg = QMessageBox()
+    msg.setWindowTitle("CoDE Warning")
+    msg.setText("The TwisTorr 305 FS pump driver has not been found, if you want to use it, then connect it to the RS485 adapter, check that it is powered on and restart the GUI...")
+    msg.setIcon(QMessageBox.Critical)
+    msg.exec_()
+    raise PumpNotFoundException("The TwisTorr 305 FS pump driver has not been found, if you want to use it, then connect it to the RS485 adapter, check that it is powered on and restart the GUI...")
+
+def apply_dark_theme(app):
+    dark_palette = QtGui.QPalette()
+    dark_palette.setColor(QtGui.QPalette.Window, QtGui.QColor(53, 53, 53))
+    dark_palette.setColor(QtGui.QPalette.WindowText, QtCore.Qt.white)
+    dark_palette.setColor(QtGui.QPalette.Base, QtGui.QColor(25, 25, 25))
+    dark_palette.setColor(QtGui.QPalette.AlternateBase, QtGui.QColor(53, 53, 53))
+    dark_palette.setColor(QtGui.QPalette.ToolTipBase, QtCore.Qt.white)
+    dark_palette.setColor(QtGui.QPalette.ToolTipText, QtCore.Qt.white)
+    dark_palette.setColor(QtGui.QPalette.Text, QtCore.Qt.white)
+    dark_palette.setColor(QtGui.QPalette.Button, QtGui.QColor(53, 53, 53))
+    dark_palette.setColor(QtGui.QPalette.ButtonText, QtCore.Qt.white)
+    dark_palette.setColor(QtGui.QPalette.BrightText, QtCore.Qt.red)
+    dark_palette.setColor(QtGui.QPalette.Link, QtGui.QColor(42, 130, 218))
+    dark_palette.setColor(QtGui.QPalette.LinkVisited, QtGui.QColor(42, 130, 218))
+    dark_palette.setColor(QtGui.QPalette.Highlight, QtGui.QColor(42, 130, 218))
+    dark_palette.setColor(QtGui.QPalette.HighlightedText, QtCore.Qt.black)
+    dark_palette.setColor(QtGui.QPalette.Disabled, QtGui.QPalette.Text, QtGui.QColor(127, 127, 127))
+    dark_palette.setColor(QtGui.QPalette.Disabled, QtGui.QPalette.ButtonText, QtGui.QColor(127, 127, 127))
+    dark_palette.setColor(QtGui.QPalette.Disabled, QtGui.QPalette.HighlightedText, QtGui.QColor(127, 127, 127))
+    app.setPalette(dark_palette)
+    app.setStyleSheet("QToolTip { color: #ffffff; background-color: #2a82da; border: 1px solid white; }")
+    
+app = QApplication(sys.argv) 
+apply_dark_theme(app)
 
 RS232 = serial.Serial(port='/dev/serial/by-id/usb-1a86_USB_Serial-if00-port0', baudrate=9600)
 
@@ -39,15 +76,22 @@ RS232.write(crc_2)
 ETX = False
 ETX_count = 0
 
+timeout = 2  
+RS232.timeout = 1 
+
 while True:
     ANS = RS232.read()
-    print(ANS, end='')  
-
-    if ETX:
-        ETX_count = ETX_count + 1
-        if ETX_count >= 2:
-            print("...")
+    if ANS:
+        print(ANS, end='')  
+        if ETX:
+            ETX_count = ETX_count + 1
+            if ETX_count >= 2:
+                print("...")
+                break
+        elif ANS == b'\x03':
+            ETX = True
+    else:
+        timeout -= 1
+        if timeout <= 0:
+            pumpNotFound()
             break
-    elif ANS == b'\x03':
-        ETX = True
-
